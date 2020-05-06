1df59b168b3ab61a13d36784fc76d74e844aa77f
locbt <locthanhbui1201@gmail.com>
2020-05-06 16:04:09 +0700

    initial commit

---
+++ linkedincourse/utilities/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(filename: str):
+    # TODO: read the text file with urls in it
+    pass
+
+
+def load_page(url: str):
+    # TODO: read the url
+    pass
+
+
+def scrape_page(contents: str):
+    # TODO: analyze the text
+    pass
